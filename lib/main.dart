1c112ada11e55e7294ddd1d04830be8bdf190ae5
ftholoza <francesco.tholozan@gmail.com>
2026-03-12 16:10:34 +0100

    debug-main

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
 import 'package:flutter/services.dart';
+import 'dart:async';
+import 'app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-
+  
   SystemChrome.setEnabledSystemUIMode(
-    SystemUiMode.immersiveSticky,
+  SystemUiMode.immersiveSticky,
   );
-
+  
+  FlutterError.onError = (FlutterErrorDetails details) {
+  FlutterError.presentError(details);
+  debugPrint('FLUTTER ERROR: ${details.exception}');
+  debugPrintStack(stackTrace: details.stack);
+  };
+  
+  runZonedGuarded(() {
   runApp(const HikariApp());
+  }, (error, stackTrace) {
+  debugPrint('ZONE ERROR: $error');
+  debugPrint('$stackTrace');
+  });
 }
